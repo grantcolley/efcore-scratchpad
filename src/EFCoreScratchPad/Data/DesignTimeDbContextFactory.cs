7d226a3bec148e9b637c44d4e4e175dafb6c830d
grantcolley <grantcolley@yahoo.com>
2022-08-18 21:30:28 +0100

    Create DesignTimeDbContextFactory.cs

---
+++ src/EFCoreScratchPad/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using EFCoreScratchPad.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Headway.Repository.Data
+{
+    /// <summary>
+    /// 
+    /// https://docs.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli
+    /// 
+    /// EF Core Tools commands (for example, the Migrations commands) require a derived DbContext 
+    /// instance to be created at design time in order to gather details about the application's 
+    /// entity types and how they map to a database schema.
+    /// 
+    /// DesignTimeDbContextFactory tells the tools how to create your DbContext.
+    /// 
+    /// </summary>
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ScratchPadContext>
+    {
+        public ScratchPadContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration
+                = new ConfigurationBuilder().SetBasePath(
+                    Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../EFCoreScratchPad/appsettings.json").Build();
+
+            var builder = new DbContextOptionsBuilder<ScratchPadContext>();            
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+                builder.UseSqlServer(connectionString);
+
+            return new ScratchPadContext(builder.Options);
+        }
+    }
+}
