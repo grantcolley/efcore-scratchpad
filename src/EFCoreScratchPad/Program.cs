7a9a65dabed08389ab52b5d9f045ddb894ba8484
grantcolley <grantcolley@yahoo.com>
2022-08-18 21:30:12 +0100

    Update Program.cs

--- src/EFCoreScratchPad/Program.cs
+++ src/EFCoreScratchPad/Program.cs
@@ -1,14 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EFCoreScratchPad.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 var config = new ConfigurationBuilder()
                .AddJsonFile("appsettings.json", false)
                .Build();
+var connectionString = config.GetConnectionString("DefaultConnection");
 
-using var dbContext = new ScratchPadContext(config.GetConnectionString("DefaultConnection"));
+Console.WriteLine($"Connection String = {connectionString}");
 
-Console.WriteLine($"Connection String = {dbContext.ConnectionString}");
+var builder = new DbContextOptionsBuilder<ScratchPadContext>();
+builder.UseSqlServer(connectionString);
+using var dbContext = new ScratchPadContext(builder.Options);
+
 Console.WriteLine();
 
 Console.ReadLine();
