7e08f0200d127c5d8681247e38416b6699af80d8
grantcolley <grantcolley@yahoo.com>
2022-08-19 09:05:30 +0100

    Update Product.cs

--- src/EFCoreScratchPad/Model/Product.cs
+++ src/EFCoreScratchPad/Model/Product.cs
@@ -1,14 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCoreScratchPad.Model
 {
     public class Product
     {
         public int ProductId { get; set; }
+        public ProductType ProductType { get; set; }
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+        public int RedressId { get; set; }
         public Redress? Redress { get; set; }
 
         [Required]
-        [StringLength(50)]
+        [StringLength(150)]
         public string? Name { get; set; }
+
+        [Required]
+        public DateTime? StartDate { get; set; }
+
+        [Range(3, 360)]
+        public int? Duration { get; set; }
+
+        [Column(TypeName = "decimal(4, 2)")]
+        public decimal? Rate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Value { get; set; }
     }
 }
