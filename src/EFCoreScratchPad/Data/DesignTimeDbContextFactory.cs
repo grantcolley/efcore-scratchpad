5021c8ca44f821d02ec57536d4b94f01acc3dbba
grantcolley <grantcolley@yahoo.com>
2022-08-19 09:05:13 +0100

    Update DesignTimeDbContextFactory.cs

--- src/EFCoreScratchPad/Data/DesignTimeDbContextFactory.cs
+++ src/EFCoreScratchPad/Data/DesignTimeDbContextFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Headway.Repository.Data
 {
