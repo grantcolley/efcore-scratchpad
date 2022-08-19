f5007a914cfcf1f0a1c3ceb801619283a640b30b
grantcolley <grantcolley@yahoo.com>
2022-08-19 09:05:39 +0100

    Update Project.cs

--- src/EFCoreScratchPad/Model/Project.cs
+++ src/EFCoreScratchPad/Model/Project.cs
@@ -1,13 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCoreScratchPad.Model
 {
     public class Project
     {
         public int ProjectId { get; set; }
+        public ProductType ProductType { get; set; }
 
         [Required]
         [StringLength(50)]
         public string? Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string? Description { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Compensation { get; set; }
+
+        [Column(TypeName = "decimal(4, 2)")]
+        public decimal? CompensatoryInterest { get; set; }
     }
 }
