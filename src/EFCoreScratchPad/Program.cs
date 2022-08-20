c6f8344acf86f460d133c871d33f03ce70e82bd7
grantcolley <grantcolley@yahoo.com>
2022-08-20 20:24:56 +0100

    Update Program.cs

--- src/EFCoreScratchPad/Program.cs
+++ src/EFCoreScratchPad/Program.cs
@@ -7,20 +7,25 @@ using System;
 var config = new ConfigurationBuilder()
                .AddJsonFile("appsettings.json", false)
                .Build();
+
 var connectionString = config.GetConnectionString("DefaultConnection");
 
 Console.WriteLine($"Connection String = {connectionString}");
 Console.WriteLine();
 
 var builder = new DbContextOptionsBuilder<ScratchPadContext>();
+builder.EnableSensitiveDataLogging(true);
 builder.UseSqlServer(connectionString);
+
 using var dbContext = new ScratchPadContext(builder.Options);
 
 Console.WriteLine("Initialise data...");
+Console.WriteLine();
 
 Data.Initialise(dbContext);
 
-Console.WriteLine("Done");
+Console.WriteLine();
+Console.WriteLine("Initialise data complete.");
 Console.WriteLine();
 
 Console.ReadLine();
