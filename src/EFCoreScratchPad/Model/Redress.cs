a5f78fdac20e7ac306eca8344598fc288825c1e3
grantcolley <grantcolley@yahoo.com>
2022-08-21 21:01:19 +0100

    Update Redress.cs

--- src/EFCoreScratchPad/Model/Redress.cs
+++ src/EFCoreScratchPad/Model/Redress.cs
@@ -5,15 +5,9 @@ namespace EFCoreScratchPad.Model
     public class Redress
     {
         public int RedressId { get; set; }
-        public int CustomerId { get; set; }
-        public Customer? Customer { get; set; }
         public int ProductId { get; set; }
         public Product? Product { get; set; }
         public int ProjectId { get; set; }
         public Project? Project { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string? Name { get; set; }
     }
 }
