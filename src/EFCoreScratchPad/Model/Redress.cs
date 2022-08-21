d96e4691433bc5d29fabeb053b00757539c83803
grantcolley <grantcolley@yahoo.com>
2022-08-21 21:38:19 +0100

    Update Redress.cs

--- src/EFCoreScratchPad/Model/Redress.cs
+++ src/EFCoreScratchPad/Model/Redress.cs
@@ -1,13 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace EFCoreScratchPad.Model
+﻿namespace EFCoreScratchPad.Model
 {
     public class Redress
     {
         public int RedressId { get; set; }
         public int ProductId { get; set; }
-        public Product? Product { get; set; }
+        public Product Product { get; set; }
         public int ProjectId { get; set; }
-        public Project? Project { get; set; }
+        public Project Project { get; set; }
     }
 }
